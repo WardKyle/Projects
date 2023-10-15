cd6cafc0435de744bb63fccb74ec300e1f1f032d
Kyle Ward <wardkyle77@gmail.com>
2023-10-15 17:57:33 -0500

    Add meme generator

---
+++ meme-generator/src/header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header(){
+  return (
+    <header>
+      <h1>Kyle Ward's Meme Generator</h1>
+      <p>React--Vite--JSX</p>
+    </header>
+  )
+}
+
+export default Header
