7f8b1e60bba435427dee9138eca0c60e0faf6b0e
Kyle Ward <wardkyle77@gmail.com>
2024-01-06 11:28:41 -0600

    Update

--- tenzies/src/components/App.jsx
+++ tenzies/src/components/App.jsx
@@ -8,6 +8,7 @@ import Confetti from 'react-confetti'
 function App(){
   const [roll, reRoll] = React.useState(randomDice()) 
   const [tenzies, setTenzies]  = React.useState(false)
+  const [count, addToCount] = React.useState(0)
 
 
   React.useEffect(() => {
@@ -32,6 +33,7 @@ if(allHeld && allSameValue)setTenzies(true)
   function reRollDice(){
     const newRoll = randomDice()
     reRoll(prevState => prevState.map(el => el.isHeld? el : newRoll[prevState.indexOf(el)]))
+    addToCount(prevState => ++prevState)
   }
 
   function Hold(id){
@@ -42,7 +44,12 @@ if(allHeld && allSameValue)setTenzies(true)
 
   function resetGame(){
     setTenzies(false)
+    addToCount(0)
     reRoll(randomDice)
+  }
+
+  const styles = {
+    color: count > 0 ? "black" : "transparent"
   }
 
   const thisRoll = roll.map(el => <Dice value={el.value} key={el.id} isHeld={el.isHeld} holdDice={()=>Hold(el.id)}/>)
@@ -52,10 +59,11 @@ if(allHeld && allSameValue)setTenzies(true)
       <div className="instructions--container">
       <h1>Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p></div>
-      <div className="dice--container">
+      <div className="roll--count" style={styles}><h6>{count}</h6></div>
+            <div className="dice--container">
         {thisRoll}
       </div>
-      {tenzies? <div><button id="roll--button" onClick={resetGame}>New Game</button><Confetti /></div> : <button id="roll--button" onClick={reRollDice}>Roll</button>}
+      {tenzies? <div><button id="roll--congrats">Congratulations! You won in {count} rolls!</button><button id="roll--button" onClick={resetGame}>New Game</button><Confetti /></div> : <div><button id="roll--button" onClick={reRollDice}>Roll</button><button id="roll--button" onClick={resetGame}>New Game</button></div>}
     </main>
   )
 }
